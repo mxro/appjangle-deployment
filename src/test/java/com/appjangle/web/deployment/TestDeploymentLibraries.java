d95390874dd6d05c6fc3adb25098974e876865f4
mxro <mxro@nowhere.com>
2014-10-03 13:59:11 +1000

    Setting up deployment tool repo Autocommit Fri Oct 03 13:59:11 EST 2014

--- src/test/java/com/appjangle/web/deployment/TestDeploymentLibraries.java
+++ src/test/java/com/appjangle/web/deployment/TestDeploymentLibraries.java
@@ -1,5 +1,14 @@
 package com.appjangle.web.deployment;
 
+import org.junit.Test;
+
+import com.appjangle.qunit.QUnit;
+
 public class TestDeploymentLibraries {
 
+	@Test
+	public void test() {
+		QUnit.run(this, "tests.html");
+	}
+	
 }
